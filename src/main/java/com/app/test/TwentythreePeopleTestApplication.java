56b93a885d831f3bcbab6d1aeb7268deb3b540eb
Franklin <franklinleones@gmail.com>
2020-08-24 18:12:21 -0400

    cambios

--- src/main/java/com/app/test/TwentythreePeopleTestApplication.java
+++ src/main/java/com/app/test/TwentythreePeopleTestApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
@@ -15,25 +14,22 @@ import com.app.test.auth.service.JWTService;
 @SpringBootApplication
 public class TwentythreePeopleTestApplication {
 
-	@Autowired
-	private JWTService jwtService;
-	
 	public static void main(String[] args) {
 		SpringApplication.run(TwentythreePeopleTestApplication.class, args);
 	}
-	
-	@EnableWebSecurity
-	@Configuration
-	class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
-		@Override
-		protected void configure(HttpSecurity http) throws Exception {
-			http.csrf().disable()
+}
+
+@Configuration
+class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Autowired
+	private JWTService jwtService;
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.csrf().disable()
 				.addFilterAfter(new JWTAuthorizationFilter(jwtService), UsernamePasswordAuthenticationFilter.class)
-				.authorizeRequests()
-				.antMatchers("/token").permitAll()
-				.anyRequest().authenticated();
-		}
+				.authorizeRequests().antMatchers("/token").permitAll().anyRequest().authenticated();
 	}
-
 }
